f04222830356728c6269b761616bcfadb36fa97a
Juan David Cruz Serrano <juandavidcruzs@gmail.com>
2016-08-10 16:21:42 -0500

    Initial Commit

---
+++ Secundo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Secundo
+//
+//  Created by Juan David Cruz Serrano on 8/10/16.
+//  Copyright © 2016 Juan David Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
